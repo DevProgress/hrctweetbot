c529d01c152ef0d59b71fe488bd8cb2b909fe032
Thavidu Ranatunga <thavidu@fellowrobots.com>
2016-10-08 23:08:56 -0700

    Testing reply with the test account #3

--- test.py
+++ test.py
@@ -3,6 +3,7 @@
 
 import tweepy, json, sys
 import wit
+import pprint
 
 config_file = str(sys.argv[1])
 fin = open(config_file)
@@ -33,10 +34,13 @@ class BotStreamListener(tweepy.StreamListener):
 			if intent=="no_reply":
 				print "I would not have replied"
 			elif intent=="email_apology":
-				response = "Have you read this? https://www.washingtonpost.com/news/post-politics/wp/2015/09/08/hillary-clinton-apologizes-for-e-mail-system-i-take-responsibility/"
+				reply = "Have you read this? https://www.washingtonpost.com/news/post-politics/wp/2015/09/08/hillary-clinton-apologizes-for-e-mail-system-i-take-responsibility/"
+				response = '@{} {}'.format(status.user.screen_name, reply)
 				if status.user.id == test_account:
+					print "Actually posting!"
 					api.update_status(response, in_reply_to_status_id = status.id)
 				print "I replied with ", response
+				pprint.pprint(status)
 		except:
 			print "Error!"
 			pass
@@ -49,3 +53,29 @@ class BotStreamListener(tweepy.StreamListener):
 botStreamListener = BotStreamListener()
 botStream = tweepy.Stream(auth = auth, listener = botStreamListener)
 botStream.filter(track = ['hillary apologized emails'])
+
+
+
+def reply_function(tweet_content):
+    return 'You said {}'.format(tweet_content)
+
+class BotStreamListener(tweepy.StreamListener):
+    # def __init__(self, reply_function):
+    #     self.reply_function = reply_function
+
+    def on_status(self, status):
+        if 'retweeted_status' in status._json:
+            pass
+        else:
+            if status.user.id == my_user_id:
+                user_mentions = filter(lambda x: x['id'] == my_user_id, status.entities['user_mentions'])
+                if len(user_mentions) > 0:
+                    username = '@{}'.format(user_mentions[0]['screen_name'])
+                    tweet_content = string.replace(status.text, username, '')
+                else:
+                    tweet_content = status.text
+                response = '@{} {}'.format(status.user.screen_name, reply_function(tweet_content))
+                api.update_status(response, in_reply_to_status_id = status.id)
+                print("\nNew tweet:")
+                print(status._json['text'])
+                print(json.dumps(status._json, indent = 4))
