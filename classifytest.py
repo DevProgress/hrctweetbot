ca53c66b990777c676182429dddf6792a2045c62
Thavidu Ranatunga <thavidu@fellowrobots.com>
2016-10-08 17:26:31 -0700

    More data and the classifer code working, just not very accurate

--- classifytest.py
+++ classifytest.py
@@ -24,7 +24,7 @@ def document_features(document):
 #print(document_features(movie_reviews.words('pos/cv957_8737.txt'))) 
 
 featuresets = [(document_features(d), c) for (d,c) in documents]
-train_set, test_set = featuresets[100:], featuresets[:100]
+train_set, test_set = featuresets[len(featuresets)/2:], featuresets[:len(featuresets)/2]
 classifier = nltk.NaiveBayesClassifier.train(train_set)
 	
 print(nltk.classify.accuracy(classifier, test_set))
